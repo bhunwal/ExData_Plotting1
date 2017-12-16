4b7c8609f13deff11de275cce8fcd97122381bbb
bhunwal <maneesh.bhunwal@gmail.com>
2017-12-16 05:49:34 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,16 @@
 plot1 <- function() {
+  
+  filename <- "household_power_consumption.zip"
+  ## Download and unzip the dataset:
+  if (!file.exists(filename)){
+    fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileURL, filename, method="curl")
+  }  
+  if (!file.exists("household_power_consumption.txt")) { 
+    unzip(filename) 
+  }
+  
+  
   mydata <- read.table("household_power_consumption.txt", sep=";", header=T) 
   head(mydata)
   mydata$Date <- as.Date(mydata$Date, "%d/%m/%Y");
