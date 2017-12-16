0eb72710b17ee67a2b414d7852d630e2d6ed27ce
bhunwal <maneesh.bhunwal@gmail.com>
2017-12-16 05:39:31 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,17 @@
+plot1 <- function() {
+  mydata <- read.table("household_power_consumption.txt", sep=";", header=T) 
+  head(mydata)
+  mydata$Date <- as.Date(mydata$Date, "%d/%m/%Y");
+  relData <- subset(mydata, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+  relData$Global_active_power <- as.integer(relData$Global_active_power)
+  relData$datetime <- as.POSIXct(paste(relData$Date, relData$Time), format="%Y-%m-%d %H:%M:%S")
+  plotGraph(relData);
+}
+
+plotGraph <- function(relData) {
+  png('rplot1.jpg')
+  hist(relData$Global_active_power, col = "red",
+              main = "Global Active Power",
+              xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+  dev.off()
+}
